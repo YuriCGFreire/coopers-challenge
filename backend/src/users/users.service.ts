2cbd1c4c1922cb6f4ee8da38c95d6be646588dcd
YuriCGFreire <yurifreireg@gmail.com>
2023-01-25 13:14:54 -0300

    Create createUser deleteUser and getUserById in user.service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import {InjectRepository} from "@nestjs/typeorm"
+import { Users } from './entities/user.entity';
+import { HttpException } from '@nestjs/common/exceptions';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(Users)
+        private readonly usersRepository: Repository<Users>
+    ){}
+
+    async createUser(createUserDto: CreateUserDto){
+        try{
+            const user = this.usersRepository.create(createUserDto)
+            await this.usersRepository.save(user)
+            return await this.usersRepository.findOne({
+                where: {id: user.id}
+            })
+        }catch(err){
+            throw new HttpException(
+                "Usuário já cadastrado.",
+                HttpStatus.BAD_REQUEST
+            )
+        }
+    }
+
+    async deleteUser(userId: string){
+        return await this.usersRepository.delete({id: userId})
+    }
+
+    async getUserById(userId: string){
+        return await this.usersRepository.findOne({
+            where: {id: userId}
+        }) 
+    }
+}
